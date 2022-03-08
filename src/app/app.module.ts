ea694830546cab9e4b96ee5cde07fd947104d0d7
Colin Herzog <colin.herzog@codin.ch>
2022-03-08 13:00:04 +0100

    config(material): adding material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { OverviewComponent } from './overview/overview.component';
-import { ModuleGroupComponent } from './components/module-group/module-group.component';
-import { ModuleClassComponent } from './components/module-class/module-class.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {OverviewComponent} from './overview/overview.component';
+import {ModuleGroupComponent} from './components/module-group/module-group.component';
+import {ModuleClassComponent} from './components/module-class/module-class.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {environment} from "../environments/environment";
 import {initializeApp, provideFirebaseApp} from "@angular/fire/app";
 import {getFirestore, provideFirestore} from "@angular/fire/firestore";
+import {getAuth, provideAuth} from "@angular/fire/auth";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 
 @NgModule({
@@ -18,6 +21,7 @@ import {getFirestore, provideFirestore} from "@angular/fire/firestore";
     OverviewComponent,
     ModuleGroupComponent,
     ModuleClassComponent,
+    NavigationComponent,
   ],
   imports: [
     BrowserModule,
@@ -26,8 +30,11 @@ import {getFirestore, provideFirestore} from "@angular/fire/firestore";
     ReactiveFormsModule,
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
