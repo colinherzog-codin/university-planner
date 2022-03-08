639c3c5275f84be6a152b390484f9ad15ea2fa4b
Colin Herzog <colin.herzog@codin.ch>
2022-03-08 10:36:16 +0100

    config(env): added env for firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBQLk5kVSdMKzprbcBt3Iz7dd5mCqEKVT8",
+    authDomain: "university-planner-ee400.firebaseapp.com",
+    projectId: "university-planner-ee400",
+    storageBucket: "university-planner-ee400.appspot.com",
+    messagingSenderId: "782212963617",
+    appId: "1:782212963617:web:81664fce7bc01e6db27260",
+    measurementId: "G-HT91PHD3ZH"
+  }
 };
